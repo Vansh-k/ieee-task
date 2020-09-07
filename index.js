2c822e1be4e37199db00e599afe03d7e8d6ae6c6
Vansh Kapoor <vanshkapoor2001@gmail.com>
2020-09-07 09:41:52 +0530

    last commit

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get("/admin",function(req,res){
 let port = process.env.PORT;
 if (port == null||port == ""){
   port=3000;
-}
+};
 
 app.listen(port, function() {
   console.log("Server has started sucessfully");
